933bd634ee3c414064fb7384a8500618048667c5
suyashkale261 <kalesuyash@gmail.com>
2021-09-22 18:12:23 +0100

    Class

---
+++ p5.php
@@ -0,0 +1,31 @@
+<?php
+class Teacher
+{
+    public $name;
+    public $email;
+    public $address;
+    public $phone;
+    public $department;
+
+    function set_teacher($name,$email,$address,$phone,$department)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->address = $address;
+        $this->phone = $phone;
+        $this->department = $department;
+    }
+
+    function get_teacher()
+    {
+        echo '<pre>'.print_r($this,true).'</pre>';
+    }
+}
+$t1 = new Teacher();
+
+if($t1 instanceof Teacher){
+    $t1->set_teacher('John','j@gmail.com','Hatfield','4739593460','Computer Science');
+    echo $t1->get_teacher();
+    echo "</br>";
+}
+?>
