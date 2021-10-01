824db3284a12f0fb04b01f023487495bfc1f8b8f
deshmukhmanas <manas.deshmukh4@gmail.com>
2021-10-02 00:06:03 +0100

    Form data manas

---
+++ formdata.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+    <h3>Form Data</h3>
+    Personal Details:- 
+    <br>
+    <br>
+    
+    <pre>
+    <b>First Name :</b>   <?php echo $_POST["fname"]; ?><br>
+    <b>Middle Name:</b>   <?php echo $_POST["mname"]; ?><br>
+    <b>Last Name:</b>     <?php echo $_POST["lname"]; ?><br>
+    <b>Father's Name:</b> <?php echo $_POST["faname"]; ?><br>
+    <b>Mother's Name:</b> <?php echo $_POST["moname"]; ?><br>
+    <b>Gender:</b>        <?php echo $_POST["gender"]; ?><br>
+    <b>Caste:</b>         <?php echo $_POST["caste"]; ?><br>
+    <b>Email-id:</b>      <?php echo $_POST["email"]; ?><br>
+    <b>Phone No</b>       <?php echo $_POST["phone"]; ?><br>
+    <b>Landline No:</b>   <?php echo $_POST["landline"]; ?><br>
+    <b>Pancard No:</b>    <?php echo $_POST["pancardno"]; ?><br>
+    <b>Date of Birth:</b> <?php echo $_POST["bday"]; ?><br>
+    <b>Address:</b>       <?php echo $_POST["message"]; ?><br>
+    <b>City:</b>          <?php echo $_POST["city"]; ?><br>
+    <b>State:</b>         <?php echo $_POST["state"]; ?><br>
+    <b>Country</b>        <?php echo $_POST["country"]; ?><br>
+    <b>Pincode:</b>       <?php echo $_POST["pincode"]; ?><br>
+    
+    </pre>
+</body>
+</html>
